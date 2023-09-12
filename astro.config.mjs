b0364ce15f6afbe1d84d947fbfec26f1722288bf
Robson Junior <robson.ga.junior@gmail.com>
2023-09-11 22:10:51 -0300

    feat: support react kbar v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import vue from '@astrojs/vue';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.robsonjunior.dev/',
+  // trailingSlash: 'always', // for server
+  integrations: [
+        react(),
+        vue(),
+        mdx(),
+        sitemap()
+    ]
 });
---
+++ src/components/react/kbar.tsx
@@ -0,0 +1,40 @@
+import {
+    KBarProvider,
+    KBarPortal,
+    KBarPositioner,
+    KBarAnimator,
+    KBarSearch
+} from "kbar";
+import  RenderResults  from './kbarResults.tsx';
+
+const actions = [
+    {
+        id: "blog",
+        name: "Blog",
+        shortcut: ["b"],
+        keywords: "writing words",
+        perform: () => (window.location.pathname = "blog")
+    },
+    {
+        id: "contact",
+        name: "Contact",
+        shortcut: ["c"],
+        keywords: "email",
+        perform: () => (window.location.pathname = "contact")
+    }
+];
+
+export default function Kbar() {
+    return (
+        <KBarProvider actions={actions}>
+            <KBarPortal>
+                <KBarPositioner>
+                    <KBarAnimator>
+                        <KBarSearch />
+                        <RenderResults />
+                    </KBarAnimator>
+                </KBarPositioner>
+            </KBarPortal>
+        </KBarProvider>
+    )
+}
