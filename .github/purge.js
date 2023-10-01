d59f3c615ff7194416489bc57abc5fa8737d7651
Robson Junior <robson.ga.junior@gmail.com>
2023-10-01 20:56:38 -0300

    chore: small purge adjust, yet disabled

--- .github/purge.js
+++ .github/purge.js
@@ -4,78 +4,76 @@ const args = require('node:process');
 const axios = require("axios");
 
 function basicToken() {
-	let token = '';
+    let token = '';
 
-	args.argv.forEach((val) => {
-		let t = val.match(/basic_token=(.*)/gi) || [];
-		if (t.length) {
-			token = t[0]
-		}
-	});
+    args.argv.forEach((val) => {
+        let t = val.match(/basic_token=(.*)/gi) || [];
+        if (t.length) {
+            token = t[0]
+        }
+    });
 
-	return token.replace(`basic_token=`, '') + '==' || null;
+    return token.replace(`basic_token=`, '') + '==' || null;
 }
 
 async function fetchPost(strUri, data, headers) {
-	const response = await axios.post(strUri, data, { headers });
-	return response;
+    const response = await axios.post(strUri, data, { headers });
+    return response;
 }
 
 async function fetchToken(strUri) {
-	const headers = {
-		"content-type": "application/json",
-		Authorization: `Basic ${basicToken()}`,
-		Accept: "application/json; version=3",
-	};
+    const headers = {
+        "content-type": "application/json",
+        Authorization: `Basic ${basicToken()}`,
+        Accept: "application/json; version=3",
+    };
 
-	const response = await fetchPost(strUri, null, headers);
+    const response = await fetchPost(strUri, null, headers);
 
-	switch (response.status) {
-		case 201:
-			console.log("[*] Token Net::HTTPCreated");
-			return response.data;
-		default:
-			console.log(`[*] ${response.statusText}`);
-			throw new Error(response.statusText);
-	}
+    switch (response.status) {
+        case 201:
+            return response.data;
+        default:
+            console.log(`[*] ${response.statusText}`);
+            throw new Error(response.statusText);
+    }
 }
 
 async function purge(strUri, token, data) {
-	console.log("[*] The PURGE is starting");
+    const headers = {
+        "content-type": "application/json",
+        Authorization: `Token ${token}`,
+        Accept: "application/json; version=3",
+    };
 
-	const headers = {
-		"content-type": "application/json",
-		Authorization: `Token ${token}`,
-		Accept: "application/json; version=3",
-	};
-
-	console.log("[*] POST headers:", headers);
-	console.log("[*] POST data:", JSON.stringify(data));
-
-	const response = await fetchPost(strUri, data, headers);
-	return response.data;
+    const response = await fetchPost(strUri, data, headers);
+    return response.data;
 }
 
 async function performPurge(TOKEN_API_URL, PURGE_WILDCARD_URL) {
-	try {
-		const tokenRequest = await fetchToken(TOKEN_API_URL);
-		const token = tokenRequest.token;
+    try {
+        const tokenRequest = await fetchToken(TOKEN_API_URL);
+        const token = tokenRequest.token;
 
-		console.log(
-			await purge(PURGE_WILDCARD_URL, token, {
-				urls: ["https://www.robsonjunior.dev/*"],
-				method: "delete",
-			})
-		);
-	} catch (error) {
-		console.error(error);
-	}
+        console.log(
+            await purge(PURGE_WILDCARD_URL, token, {
+                urls: [
+                    "https://www.robsongajunior.dev/*",
+                    "https://www.robsonjunior.dev/*",
+                    "https://www.rjr.dev/*"
+                ],
+                method: "delete",
+            })
+        );
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 (function main() {
-	const API_HOST = 'api.azionapi.net';
-	const TOKEN_API_URL = `https://${API_HOST}/tokens`;
-	const PURGE_WILDCARD_URL = `https://${API_HOST}/purge/wildcard`;
+    const API_HOST = 'api.azionapi.net';
+    const TOKEN_API_URL = `https://${API_HOST}/tokens`;
+    const PURGE_WILDCARD_URL = `https://${API_HOST}/purge/wildcard`;
 
-	performPurge(TOKEN_API_URL, PURGE_WILDCARD_URL);
+    performPurge(TOKEN_API_URL, PURGE_WILDCARD_URL);
 })();
