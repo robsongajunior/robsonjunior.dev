0d900b125fde40e6aded968f02e3148bbf7205da
Robson Junior <robson.ga.junior@gmail.com>
2023-10-15 17:56:25 -0300

    WIP: merge, untropic branch

--- src/components/react/Kbar/KbarButtonHook.tsx
+++
@@ -1,9 +0,0 @@
-import { useKbar } from "kbar";
-
-export default function KbarButtonHook() {
-  const { query, disabled } = useKbar(state => ({
-    disabled: state.disabled
-  }));
-
-  return <button onClick={() => query.disable(!disabled)}>{disabled ? "Disabled" : "Disable"}</button>
-}
