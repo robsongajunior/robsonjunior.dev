3fd0a1780d6e82359549cddfc2f6caf037115d51
Robson Junior <robson.ga.junior@gmail.com>
2023-11-12 18:11:08 -0300

    feat: header buguer and scroll beehavior

---
+++ src/components/vue/NavigationRight.vue
@@ -0,0 +1,110 @@
+<template>
+    <aside id="navigationRight" :class="{ active: isActive }">
+        <ul>
+            <li>
+                <a href="/blog/">
+                    blog
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/robsongajunior/">
+                    github
+                </a>
+            </li>
+            <li>
+                <a href="/sobre/">
+                    sobre
+                </a>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<script setup>
+    import { onMounted } from 'vue';
+    import { useStore } from '@nanostores/vue';
+    import { isMenuActive } from '../../nanostores/BurguerButton.js';
+
+    let isActive = useStore(isMenuActive);
+
+    onMounted(() => {
+        (function () {
+            let lastScrollTop = 0;
+            let showHeader = true;
+            let f = {};
+            let navigationRight = document.querySelector("#navigationRight");
+
+            f.up = function () {
+                if (showHeader === true) return;
+                navigationRight.style.top = '80px';
+                navigationRight.style.height = 'calc(100vh - 80px)';
+                showHeader = true;
+            };
+
+            f.down = function () {
+                if (showHeader === false) return;
+                navigationRight.style.top = '0';
+                navigationRight.style.height = '100vh';
+                showHeader = false;
+            };
+
+            window.addEventListener("scroll", function (e) {
+                if (e.scrollY === lastScrollTop) return;
+
+                let direction = this.scrollY < lastScrollTop ? "up" : "down";
+                lastScrollTop = this.scrollY;
+
+                if (typeof f[direction] !== "function") return;
+
+                f[direction]();
+            }, true);
+        })();
+    });
+</script>
+
+<style lang="scss" scoped>
+    aside {
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.5);
+        backdrop-filter: blur(10px);
+        width: 0;
+        max-width: calc(100% - 1rem);
+        height: calc(100vh - 80px);
+        z-index: 10;
+        transition: var(--transition-all-ease-in-out);
+
+        &.active {
+            width: 360px;
+            padding: 1rem;
+
+            ul li a {
+                opacity: 1;
+            }
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+
+            li {
+                a {
+                    opacity: 0;
+                    transition: var(--transition-all-ease-in-out);
+                    padding: .5rem 0.5rem;
+                    color: var(--median);
+                    border-bottom: 1px solid transparent;
+                    text-decoration: none;
+                    font-size: 0.8rem;
+
+                    &.active {
+                        text-decoration: none;
+                        color: var(--dark);
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ src/nanostores/BurguerButton.js
@@ -0,0 +1,3 @@
+import { atom } from 'nanostores'
+
+export let isMenuActive = atom(false);
