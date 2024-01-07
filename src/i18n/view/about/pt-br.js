409085537ee2a547f682659654162797eb98c71c
Robson Junior <robson.ga.junior@gmail.com>
2024-01-07 18:38:30 -0300

    chore: finish about i18n structure

--- src/i18n/view/about/pt-br.js
+++ src/i18n/view/about/pt-br.js
@@ -63,6 +63,43 @@ export default {
                 <a target=\"_blank\" href=\"https://www.google.com/search?gs_ssp=eJzj4tDP1TcwSc9LM2D04svJzCstVijJLypLzEkpBgBq1wjA&amp;q=linus+torvalds&amp;rlz=1C5CHFA_enBR1040BR1040&amp;oq=Linus+&amp;gs_lcrp=EgZjaHJvbWUqCggCEC4YsQMYgAQyBggAEEUYOTIKCAEQLhixAxiABDIKCAIQLhixAxiABDINCAMQABiDARixAxiABDIHCAQQABiABDIHCAUQABiABDIHCAYQLhiABDIHCAcQABiABDINCAgQLhjHARjRAxiABDIHCAkQABiPAtIBCTE0MTAzajBqN6gCALACAA&amp;sourceid=chrome&amp;ie=UTF-8\">Linus Torvalds</a>: \
                 <br> \
                 <strong>- Talk is cheap, show me the code.</strong>"
+            ],
+            cards: [
+                {
+                    title: "Azion Pulse Live Test",
+                    description: "Live Test do <a href=\"https://www.azion.com/pt-br/produtos/edge-pulse/\" target=\"_blank\">Azion Pulse</a>. Dados da última milha mostrado de forma visual, dados de client, resolver e  timings por Edge Location executando um teste de Avaiability e Response Time.",
+                    link: "ver projeto",
+                    linkHrefTitle: "Azion Pulse Live Test",
+                    linkHref: "https://www.azion.com/pt-br/produtos/edge-pulse/teste/",
+                },
+                {
+                    title: "Azion Docs",
+                    description: "Migração de Jekyll para Astro com objetivo de se tornar agnóstico a component library, suportanto mdx e dispobibilizando novos componntes para serem facilmente consumidos pelos writes.",
+                    link: "ver projeto",
+                    linkHrefTitle: "Azion Docs",
+                    linkHref: "https://docs.azion.com/",
+                },
+                {
+                    title: "Ask Azion",
+                    description: "Ask Azion é uma Inteligencia Artifical ainda no modo Beta com finalidades experimentais e de aprendizado para melhorarmos pontos importantes de documentação, atendimento, getting started e vários outros interesses.",
+                    link: "ver projeto",
+                    linkHrefTitle: "Ask Azion",
+                    linkHref: "https://www.azion.com/en/ask-azion",
+                },
+                {
+                    title: "Resource Hub",
+                    description: "Com uma arquitetura JAM Stack primeiro projeto da Azion utilizando Astro, agnóstico component library consumindo Azion Edge Functions API. Um Portal de recursos de gerados pela Azion.",
+                    link: "ver projeto",
+                    linkHrefTitle: "Azion Resource Hub",
+                    linkHref: "https://www.azion.com/pt-br/resource-hub/",
+                },
+                {
+                    title: "Live map",
+                    description: "Projeto iniciado na Black Friday 2022 com objetivo de um formato fácil temos uma interpretação de imporantes dados ocorrendo sobre infra-estrutura da Azion.",
+                    link: "ver projeto",
+                    linkHrefTitle: "Azion Live Map",
+                    linkHref: "https://www.azion.com/pt-br/live-map/",
+                }
             ]
         },
         talks: {
@@ -80,6 +117,36 @@ export default {
                 "Também fui também como prestigiador: \
                 <br> \
                 JS Conf Argentina, Front-In-Aracaju, Front-In-Sampa, Criciuma Dev, Python Brasil, TDC, RSJS e por aí vai indo, lista é grande!"
+            ],
+            cards: [
+                {
+                    title: "Move to the Edge - Construindo Aplicações Modernas e Escaláveis",
+                    description: "Marcando o retorno do <a target=\"https://frontinpoa.com.br/\" title=\"Front In Poa\">Front In Poa</a> 2023, essa apresentação vimos rodar aplicações no Edge e seus facilitadores junto com a importância do Front-End para o Edge Computing.",
+                    link: "ver apresentação",
+                    linkHrefTitle: "Apresentação: Move to the Edge - Construindo Aplicações Modernas e Escaláveis",
+                    linkHref: "https://speakerdeck.com/robsongajunior/move-to-the-edge-construindo-aplicacoes-modernas-e-escalaveis",
+                },
+                {
+                    title: "Construindo o futuro da web com JAM Stack",
+                    description: "Apresentação abordando como JAM Stack é usado e tem ajudado e melhorar o desacoplamento entre regras de negócio, interface, camada de dados fechando com live code utilizando a Azion Technologies.",
+                    link: "ver apresentação",
+                    linkHrefTitle: "Apresentação: Construindo o futuro da web com JAM Stack",
+                    linkHref: "https://speakerdeck.com/robsongajunior/construindo-o-futuro-da-web-com-jam-stack",
+                },
+                {
+                    title: "Como JAM Stack ajudou crescer o time de Marketing na Azion",
+                    description: "Essa apresentação tem o objetivo falar sobre arquitetura JAM Stack usada na Azion. Será explicado \"<strong>o que é JAM Stack</strong>\", mostrado um casos de uso, também falaremos sobre problemas/soluções e como montamos essa arquitetura estática para o time de Marketing ter um processo mais ágil na criação de páginas de presença web.",
+                    link: "ver apresentação",
+                    linkHrefTitle: "Apresentação: Como JAM Stack ajudou crescer o time de Marketing na Azion",
+                    linkHref: "https://speakerdeck.com/robsongajunior/como-jam-stack-ajudou-crescer-o-time-de-marketing-na-azion",
+                },
+                {
+                    title: "O pensamento por trás de um desenvolvimento escalável.",
+                    description: "Com uma arquitetura JAM Stack primeiro projeto da Azion utilizando Astro, agnóstico component library consumindo Azion Edge Functions API.",
+                    link: "ver apresentação",
+                    linkHrefTitle: "Apresentação: O pensamento por trás de um desenvolvimento escalável.",
+                    linkHref: "https://speakerdeck.com/robsongajunior/o-pensamento-por-tras-de-um-desenvolvimento-escalavel",
+                }
             ]
         }
     }
