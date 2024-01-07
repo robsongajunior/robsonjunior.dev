ffa491b01b2ec29d0d099d3e28c1e78a7aeb974b
Robson Junior <robson.ga.junior@gmail.com>
2024-01-07 18:54:13 -0300

    chore: add index.js to i18n components - add footer content

---
+++ src/i18n/component/footer/pt-br.js
@@ -0,0 +1,3 @@
+export default {
+    text: "Construído com <a href=\"https://astro.build/\" target=\"_blank\">Astro</a> - &copy; 2024 Robson Júnior. Licensa <a href=\"https://github.com/robsongajunior/robsonjunior.dev/blob/main/LICENSE\" target=\"_blank\" title=\"MIT License\">MIT</a>."
+};
