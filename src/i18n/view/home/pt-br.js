763a517f2154dab9d5d1430fb6e2b54347fd3a26
Robson Junior <robson.ga.junior@gmail.com>
2024-03-31 00:22:42 -0300

    feat: adding pulse css effect

--- src/i18n/view/home/pt-br.js
+++ src/i18n/view/home/pt-br.js
@@ -10,7 +10,7 @@ export default {
     page: {
         hero: {
             title: "<strong><span class='letter'>B</span><span class='letter'>O</span><span class='letter'>R</span><span class='letter'>N</span></strong> <span class='letter'>t</span><span class='letter'>o</span> <strong><span class='letter'>B</span><span class='letter'>U</span><span class='letter'>I</span><span class='letter'>L</span><span class='letter'>D</span></strong><br><span class='letter'>a</span><span class='letter'>n</span><span class='letter'>d</span> <strong><span class='letter'>D</span><span class='letter'>E</span><span class='letter'>L</span><span class='letter'>I</span><span class='letter'>V</span><span class='letter'>E</span><span class='letter'>R</span></strong>",
-            description: "Pressione <kbd id=\"keyCommand\">⌘</kbd><kbd>k</kbd> para começar <strongc>→</strong>"
+            description: "Pressione <kbd id=\"keyCommand\">⌘</kbd><kbd>k</kbd> para começar <strong class='pulsing-effect'>→</strong>"
         }
     }
 };
